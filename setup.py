c71a192b14bab4469be5144bc53ba00a9681e203
Sébastien Copin <cosad3s@outlook.com>
2024-02-20 10:49:16 +0100

    Up version for dependency fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 from os import path
 import io
 
-VERSION = '1.0.1' 
+VERSION = '1.0.2' 
 DESCRIPTION = 'hfinder'
 
 pwd = path.abspath(path.dirname(__file__))
